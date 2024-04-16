50b5a935d515c54067da7f43b0230b8532b95a9f
Raknaros <giumarchand@gmail.com>
2024-04-16 01:21:53 -0500

    first commit

---
+++ LoadIQBF.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+iqbf = pd.read_excel('C:\\Users\\Raknaros\\Downloads\\DJRO_20548030529_202212.xls', sheet_name=[0,3], skiprows=5,
+                     skipfooter=5, usecols='B:AG', date_format='%d/%m/%Y', parse_dates=[2, ], header=None,
+                     na_values=' ')
+#iqbf.iloc[:, 3:27] = iqbf.iloc[:, 3:27].astype(object, errors='ignore')
+
+
+# iqbf = iqbf.astype({'Equipo/Maquinaria': object, 'Placa Equipo Maquinaria ': object})
+# iqbf['Fecha']=pd.to_datetime(iqbf['Fecha'], format='%d/%m/%Y')
+# iqbf.info()
+
+def load_iqbf(carpeta):
+    set_iqbf = pd.DataFrame(
+        columns=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18',
+                 '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'])
+    for i in os.listdir(carpeta):
+        iqbf = pd.read_excel('C:\\Users\\Raknaros\\Downloads\\' + i, sheet_name=[0,3], skiprows=5,
+                             skipfooter=5, usecols='B:AG', date_format='%d/%m/%Y', parse_dates=[2, ], header=None,
+                             na_values=' ')
+        set_iqbf = set_iqbf._append([iqbf[0], iqbf[3]], ignore_index=True)
+    set_iqbf.iloc[:, 3:27] = set_iqbf.iloc[:, 3:27].astype(object, errors='ignore')
+
+load_iqbf()
