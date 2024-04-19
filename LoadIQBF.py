508511362f14f444451bececaa055a9836cd97d9
Raknaros <giumarchand@gmail.com>
2024-04-19 01:15:55 -0500

    to_sql ready

--- LoadIQBF.py
+++ LoadIQBF.py
@@ -1,27 +1,36 @@
 import pandas as pd
 import os
+from sqlalchemy import create_engine
+import psycopg2
 
-#pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-#iqbf = pd.read_excel('C:\\Users\\Raknaros\\Desktop\\temporal\\DJRO_20548030529_202212.xls', sheet_name=3, skiprows=5,
-#                     skipfooter=5, usecols='B:AG', date_format='%d/%m/%Y', parse_dates=[2, ], header=None,
-#                     na_values=' ')
-#iqbf.iloc[:, 3:27] = iqbf.iloc[:, 3:27].astype(object, errors='ignore')
 
-
-# iqbf = iqbf.astype({'Equipo/Maquinaria': object, 'Placa Equipo Maquinaria ': object})
-# iqbf['Fecha']=pd.to_datetime(iqbf['Fecha'], format='%d/%m/%Y')
-#print(iqbf)
-#columns=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18','19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32']
 def load_iqbf(carpeta):
     set_iqbf = pd.DataFrame()
+    engine = create_engine('postgresql://admindb:72656770@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com'
+                           ':5432/warehouse')
     for i in os.listdir(carpeta):
-        iqbf = pd.read_excel('C:\\Users\\Raknaros\\Desktop\\temporal\\' + i, sheet_name=[0,3], skiprows=5,
+        iqbf = pd.read_excel('C:\\Users\\Raknaros\\Desktop\\temporal\\' + i, sheet_name=[0, 3], skiprows=5,
                              skipfooter=5, usecols='B:AG', date_format='%d/%m/%Y', parse_dates=[2, ], header=None,
                              na_values=' ')
-        set_iqbf = set_iqbf._append([iqbf[0].assign(ruc=i[5:16]),iqbf[3].assign(ruc=i[5:16])], ignore_index=True)
+        set_iqbf = set_iqbf._append([iqbf[0].assign(ruc=i[5:16]), iqbf[3].assign(ruc=i[5:16])], ignore_index=True)
     set_iqbf.iloc[:, 3:27] = set_iqbf.iloc[:, 3:27].astype(object, errors='ignore')
-    return print(set_iqbf)
+    set_iqbf.rename(
+        columns={1: 'nro_operacion', 2: 'fecha', 3: 'periodo_mensual', 4: 'establecimiento', 5: 'tipo_operacion',
+                 6: 'actividad',
+                 7: 'tipo_servicio', 8: 'transaccion', 9: 'transferente', 10: 'adquiriente', 11: 'datos_propietario',
+                 12: 'datos_destinatario',
+                 13: 'traslado_guia', 14: 'sustento', 15: 'guia', 16: 'nro_matricula_transporte',
+                 17: 'nombre_transporte', 18: 'equipo_maquinaria',
+                 19: 'placa_equipo_maquinaria', 20: 'codigo_artesano', 21: 'tipo_documento_precedente',
+                 22: 'nro_documento_precedente',
+                 23: 'relacionado_bien', 24: 'documento', 25: 'dam', 26: 'bien', 27: 'cantidad', 28: 'presentacion',
+                 29: 'cantidad_presentacion', 30: 'merma', 31: 'cantidad_neta_total', 32: 'observaciones'},
+        inplace=True)
+    # set_iqbf.to_sql('iqbf', engine, if_exists='append', index=False,schema='acc')
+    return print(set_iqbf.to_sql('iqbf', engine, if_exists='append', index=False, schema='acc'))
+
 
 load_iqbf('C:\\Users\\Raknaros\\Desktop\\temporal')
