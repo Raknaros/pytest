9f4045e71ceb3fb588d9b4c417a1a9d88a949545
raknaros <giumarchand@gmail.com>
2024-04-27 12:33:17 -0500

    Other forms in develop

--- LoadF0621.py
+++ LoadF0621.py
@@ -7,11 +7,24 @@ import zipfile
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-archivo_zip = 'tus_archivos.zip'
 
-nombre_archivo_csv = 'archivo.csv'
+def load_f0621(carpeta: str):
+    set_f0621 = pd.Dataframe()
+    engine = create_engine('postgresql://admindb:72656770@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com'
+                           ':5432/warehouse')
+    for i in list(filter(lambda x: (x[12:16] == '0621'), os.listdir(carpeta))):
+        with zipfile.ZipFile(carpeta+'\\' + i, 'r') as zipf:
+            with zipf.open(i[34:42]+i[0:11]+'pdt621_casillas.csv') as archivo_csv:
+                f0621 = pd.read_csv(archivo_csv, sep=',')
+        set_f0621 = set_f0621._append(f0621[0].assign(ruc=i[5:16]), ignore_index=True)
 
-with zipfile.ZipFile(archivo_zip, 'r') as zipf:
-    with zipf.open(nombre_archivo_csv) as archivo_csv:
-        # Cargar el archivo CSV en un DataFrame de pandas
-        df = pd.read_csv(archivo_csv)
+
+
+
+my_list = [12, 65, 54, 39, 102, 339, 221, 50, 70, ]
+my_list2 = ['ba', 'ca', 'da', 'ea', 'jd', 'kd', ]
+# use anonymous function to filter and comparing
+# if divisible or not
+result = list(filter(lambda x: (x[1] == 'a'), my_list2))
+
+print(result)
