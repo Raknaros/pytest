8bfb8b6f2e7a0d8912f63f747bd0be8eb8e67b5a
Raknaros <giumarchand@gmail.com>
2024-04-28 12:10:04 -0500

    F0621 road to finish

--- LoadF0621.py
+++ LoadF0621.py
@@ -8,23 +8,24 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
+# .assign(ruc=i[0:11])
 def load_f0621(carpeta: str):
-    set_f0621 = pd.Dataframe()
+    set_f0621 = pd.DataFrame()
     engine = create_engine('postgresql://admindb:72656770@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com'
                            ':5432/warehouse')
     for i in list(filter(lambda x: (x[12:16] == '0621'), os.listdir(carpeta))):
-        with zipfile.ZipFile(carpeta+'\\' + i, 'r') as zipf:
-            with zipf.open(i[34:42]+i[0:11]+'pdt621_casillas.csv') as archivo_csv:
-                f0621 = pd.read_csv(archivo_csv, sep=',')
-        set_f0621 = set_f0621._append(f0621[0].assign(ruc=i[5:16]), ignore_index=True)
+        with (zipfile.ZipFile(carpeta + '/' + i, 'r') as zipf):
+            with zipf.open(i[28:36] + i[0:11] + 'pdt621_casillas.csv') as archivo_csv:
+                f0621 = pd.read_csv(archivo_csv, usecols=[6, 7], header=0).T.reset_index(drop=True)
+                f0621.columns = f0621.iloc[0]
+                f0621 = f0621[1:].add_prefix('_')#.drop(
+                    #['_5', '_46', '_56', '_58', '_509', '_510', '_511', '_861', '_886', '_887', '_888', '_889',
+                     #'_895'], axis=1).assign(numero_orden=i[17:27])
+                f0621.rename(columns={'_2': 'ruc', '_7': 'periodo_tributario', '_13': 'fecha_presentacion'},
+                             inplace=True)
+        set_f0621 = set_f0621._append(f0621, ignore_index=True)
+        #pendiente probar metodo merge
+    return print(set_f0621)#.to_sql('_9', engine, if_exists='append', index=False, schema='acc')
 
 
-
-
-my_list = [12, 65, 54, 39, 102, 339, 221, 50, 70, ]
-my_list2 = ['ba', 'ca', 'da', 'ea', 'jd', 'kd', ]
-# use anonymous function to filter and comparing
-# if divisible or not
-result = list(filter(lambda x: (x[1] == 'a'), my_list2))
-
-print(result)
+load_f0621('C:/Users/Raknaros/Desktop/temporal/declaraciones')
--- LoadF0710.py
+++ LoadF0710.py
@@ -4,15 +4,31 @@ from sqlalchemy import create_engine
 import psycopg2
 import zipfile
 
-# Ruta del archivo ZIP
-archivo_zip = 'tus_archivos.zip'
+df1 = pd.DataFrame({
+    'col1': ['a', 'b', 'c'],
+    'col2': [1, 2, 3],
+    'col3': ['x', 'y', 'z']
+})
 
-# Nombre del archivo CSV dentro del ZIP
-nombre_archivo_csv = 'archivo.csv'
+df2 = pd.DataFrame({
+    'col1': ['c', 'e', 'f'],
+    'col2': [4, 5, 6],
+    'col4': ['u', 'v', 'w']
+})
 
-# Crear un objeto ZipFile
-with zipfile.ZipFile(archivo_zip, 'r') as zipf:
-    # Leer el archivo CSV directamente desde el archivo ZIP
-    with zipf.open(nombre_archivo_csv) as archivo_csv:
-        # Cargar el archivo CSV en un DataFrame de pandas
-        df = pd.read_csv(archivo_csv)
+# Unir los dataframes
+df_unido = pd.merge(df1, df2, how='outer')
+
+print(df_unido)
+
+
+dfs = [df1, df2, df3, ..., df50]  # reemplaza esto con tus dataframes
+
+# Inicializa el dataframe final con el primer dataframe de la lista
+df_final = dfs[0]
+
+# Itera sobre los dataframes restantes y únelos al dataframe final
+for df in dfs[1:]:
+    df_final = pd.merge(df_final, df, on=['col1', 'col2'], how='outer')
+
+print(df_final)
