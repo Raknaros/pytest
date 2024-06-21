5d9bff7d6381feb745be07ba44082513706b8f50
Raknaros <giumarchand@gmail.com>
2024-06-21 15:19:18 -0500

    Pedidos y facturas rdy

--- GetEmitir.py
+++ GetEmitir.py
@@ -9,39 +9,38 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-def tofacturas(proveedor: str):
+def tofacturas(proveedor: str, dias: str):
     salessystem = create_engine(
         'mysql+pymysql://admin:Giu72656770@sales-system.c988owwqmmkd.us-east-1.rds.amazonaws.com'
         ':3306/salessystem')
 
+    facturas = pd.read_sql("SELECT * FROM lista_facturas", con=salessystem,
+                           parse_dates=['emision', 'vencimiento', 'vencimiento2', 'vencimiento3', 'vencimiento4'])
 
-    facturas = pd.read_sql("SELECT * FROM lista_facturas WHERE alias = '" + proveedor + "'", con= salessystem, parse_dates=['emision', 'vencimiento', 'vencimiento2', 'vencimiento3', 'vencimiento4'])
+    guias = pd.read_sql("SELECT * FROM lista_guias", salessystem, index_col='cui',
+                        parse_dates=['traslado'])
 
-    guias = pd.read_sql("SELECT * FROM lista_guias WHERE alias = '" + proveedor + "'", salessystem, index_col='cui', parse_dates=['traslado'])
+    with pd.ExcelWriter('output_with_multiple_sheets.xlsx', engine='xlsxwriter') as writer:
+        lista = pd.pivot_table(facturas, values=["sub_total", "igv", "total", "vencimiento", "moneda"],
+                               index=['cui', 'guia', 'numero', 'emision', 'descripcion', 'unidad_medida', 'cantidad',
+                                      'p_unit'],
+                               aggfunc={'sub_total': 'sum', 'igv': 'sum', 'total': 'sum', 'vencimiento': 'first',
+                                        'moneda': 'first'})
 
-    #guias.set_index("cui", inplace=True)
+        lista = lista[['sub_total', 'igv', 'total', 'vencimiento', 'moneda']]
+        lista = lista.sort_index(level='emision')
+        #TODO ORDENAR POR FECHA DE EMISION (CONSIDERAR QUE ES UN INDICE)
 
-    #emitir.style.format({'cantidad': '{:.0f}', 'precio_unit': '{:.3f}'})
+        lista = pd.concat([
+            y._append(y[['sub_total', 'igv', 'total']].sum().rename((x, guias.at[x, 'placa'], guias.at[x, 'conductor'],
+                                                                     guias.at[x, 'traslado'], guias.at[x, 'llegada'],
+                                                                     guias.at[x, 'datos_adicionales'], '', 'Totales')))
+            for x, y in lista.groupby(level=0)
+        ])
+        print(lista.index.names)
+        #Considerar agregar multihoja por proveedor
 
-    lista = pd.pivot_table(facturas, values=["sub_total", "igv", "total", "vencimiento", "moneda"],
-                           index=['cui', 'guia', 'numero', 'emision', 'descripcion', 'unidad_medida', 'cantidad',
-                                  'p_unit'],
-                           aggfunc={'sub_total': 'sum', 'igv': 'sum', 'total': 'sum', 'vencimiento': 'first',
-                                    'moneda': 'first'})
-
-    lista = lista[['sub_total', 'igv', 'total', 'vencimiento', 'moneda']]
-    lista = lista.sort_index(level='emision')
-    #TODO ORDENAR POR FECHA DE EMISION (CONSIDERAR QUE ES UN INDICE)
-
-    lista = pd.concat([
-        y._append(y[['sub_total', 'igv', 'total']].sum().rename((x, guias.at[x, 'placa'], guias.at[x, 'conductor'],
-                                                                 guias.at[x, 'traslado'], guias.at[x, 'llegada'],
-                                                                 guias.at[x, 'datos_adicionales'], '', 'Totales')))
-        for x, y in lista.groupby(level=0)
-    ])
-    print(lista.index.names)
-    #Considerar agregar multihoja por proveedor
-    return lista.to_excel('lista_emision.xlsx', sheet_name='emitir', float_format='%.3f')
+        #return lista.to_excel('lista_emision.xlsx', sheet_name='emitir', float_format='%.3f')
 
 
 #TODO agregar parametro rango de fechas, formatear la fecha y asegurar los 3 decimales en caso de ser necesario
