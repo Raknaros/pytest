c68522bf18ebdd3a581430a6b7b3f1024394f40a
Raknaros <giumarchand@gmail.com>
2024-11-20 09:15:56 -0500

    Pedidos y facturas rdy

--- test.py
+++ test.py
@@ -10,14 +10,13 @@ salessystem = create_engine(
         'mysql+pymysql://admin:Giu72656770@sales-system.c988owwqmmkd.us-east-1.rds.amazonaws.com'
         ':3306/salessystem')
 
-customers = pd.read_sql('select * from customers', salessystem)
-#nuevos_customers = pd.read_excel('C:/Users/Raknaros/Desktop/Out_54.xlsx', sheet_name='Sheet1')
-#nuevos_customers.drop(['Unnamed: 0','Unnamed: 1','Unnamed: 2'], axis=1, inplace=True)
+warehouse = create_engine('postgresql://admindb:72656770@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com'
+                              ':5432/warehouse')
 
+catalogo = pd.read_sql('SELECT * FROM catalogo', salessystem)
 
+pre_detalle =pd.read_sql('SELECT * FROM pre_detalle ORDER BY fecha_emision DESC LIMIT 200', warehouse)
 
-#nuevos_customers.dropna(axis=0, inplace=True)
+dfResultado = pd.merge(pre_detalle, catalogo, on='descripcion', how='left')
 
-#print(nuevos_customers)
-
-print(customers.loc[customers['ruc'] == 10078587909, 'alias'].values[0])
+print(dfResultado.head(20))
