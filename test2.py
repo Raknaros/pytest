7d4f5447971e3e81c4d8966e25ee0da8cf136396
Raknaros <giumarchand@gmail.com>
2025-01-19 21:52:49 -0500

    Pedidos y facturas rdy

--- test2.py
+++ test2.py
@@ -1,88 +1,24 @@
-import pandas as pd
-import os
-from sqlalchemy import create_engine
-import numpy as np
-import pypdf
-from pypdf import PdfReader, PdfWriter, PdfMerger
-import fnmatch
+from telegram import Bot
+import asyncio
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+# Token de tu bot
+BOT_TOKEN = "7563751301:AAHwAqNvDLcF3OWWvPoElpwuPGU3DSKg__o"
+# Tu chat ID
+CHAT_ID = "1340578043"
 
-warehouse = create_engine('postgresql://admindb:72656770@datawarehouse.cgvmexzrrsgs.us-east-1.rds.amazonaws.com'
-                          ':5432/warehouse')
+# Mensaje a enviar
+MESSAGE = "Hola Gordinfla"
 
-salessystem = create_engine('mysql+pymysql://admin:Giu72656770@sales-system.c988owwqmmkd.us-east-1.rds.amazonaws.com'
-                            ':3306/salessystem')
+async def send_message():
+    bot = Bot(token=BOT_TOKEN)
+    try:
+        await bot.send_message(chat_id=CHAT_ID, text=MESSAGE)
+        print("Mensaje enviado correctamente!")
+    except Exception as e:
+        print(f"Error al enviar mensaje: {e}")
 
-ruta = 'C:/Users/Raknaros/Downloads/pdfpedidosoctubre/pdfpedidosoctubre'
-
-periodo = "202410"
-
-directorio = 'C:\\Users\\Raknaros\\Desktop\\temporal\\pdfpedidosoctubre'
-
-# Obtener lista de archivos PDF en el directorio
-archivos = [archivo for archivo in os.listdir(directorio) if archivo.endswith('.pdf')]
-
-facturas_noanuladas = pd.read_sql(
-    "SELECT ruc AS proveedor,numero_serie AS serie, numero_correlativo AS correlativo, numero_documento AS adquiriente, tipo_documento_referencia, numero_documento_referencia FROM facturas_noanuladas WHERE periodo_tributario = " + periodo + " ORDER BY adquiriente, proveedor, correlativo",
-    dtype={'proveedor': str, 'adquiriente': str, 'correlativo': str, 'numero_documento_referencia': str}, con=warehouse)
-
-facturas_noanuladas['guia'] = np.where(
-    (facturas_noanuladas['tipo_documento_referencia'] == 9) &
-    (facturas_noanuladas['numero_documento_referencia'].str[:4] == 'EG07'),
-    facturas_noanuladas['numero_documento_referencia'].str.split('-').str[1].str.replace('|', '').astype(int).astype(
-        str),
-    None
-)
-
-proveedores = pd.read_sql("SELECT numero_documento AS proveedor, alias FROM proveedores", con=salessystem,
-                          dtype_backend="pyarrow")
-
-customers = pd.read_sql("SELECT related_user, CAST(ruc AS CHAR) AS adquiriente, alias FROM customers", con=salessystem,
-                        dtype_backend="pyarrow")
-
-lista_filtrada = facturas_noanuladas[~facturas_noanuladas['adquiriente'].isin(proveedores['proveedor'].astype(str))]
-
-adquirientes = lista_filtrada['adquiriente'].unique()
-
-lista = pd.merge(lista_filtrada, customers, on='adquiriente', how='left')
-
-for adquiriente in adquirientes:
-    merger = PdfWriter()
-    #Filtrar el Dataframe por adquiriente
-    lista_adquiriente = lista[lista['adquiriente'] == adquiriente]
-    print(f'Pedido de {lista_adquiriente['alias'].iloc[0]}')
-
-    for index, row in lista_adquiriente.iterrows():
-        expresiones = [
-            f'PDF-DOC-{row['serie']}-{row['correlativo']}{row['proveedor']}*',
-            f'PDF-DOC-{row['serie']}{row['correlativo']}{row['proveedor']}*',
-            f'{row['proveedor']}-01-{row['serie']}-{row['correlativo']}*'
-        ]
-
-        factura = None
-        for expresion in expresiones:
-            coincidencias = fnmatch.filter(archivos, expresion)
-            if coincidencias:
-                factura = coincidencias[0]
-                break
-
-        if factura:
-            merger.append(os.path.join(directorio, factura))
-        else:
-            print(f'Falta la factura {row['serie']}-{row['correlativo']} de {row['proveedor']}')
-
-        if row['guia'] is not None and row['guia'] != 'None' and not pd.isna(row['guia']):
-            guia = fnmatch.filter(archivos, f'{row['proveedor']}-09-EG07-{row['guia']}*')
-            if guia:
-                merger.append(os.path.join(directorio, guia[0]))
-            else:
-                print(f'Falta la guia EG07-{row['guia']} de {row['proveedor']}')
-
-    merger.write(f'{periodo}_{adquiriente}_{lista_adquiriente['alias'].iloc[0]}.pdf')
-    merger.close()
-
+# Ejecutar la función asíncrona
+asyncio.run(send_message())
 #TODO VERIFICAR LA CONSISTENCIA DE LA DATA DE CIERRE DE MES EN LOS SIGUIENTES SENTIDOS:
 
 
@@ -119,3 +55,30 @@ for adquiriente in adquirientes:
 #TODO BUSCAR FORMA DE IDENTIFICAR _5 CON FACTURAS QUE TIENEN PENDIENTE EL NUMERO DE CORRELATIVO Y DE GUIA SI FUESE NECESARIO
 #TRANSFERIR TAMBIEN LAS GUIAS
 #TODO TRANSFERIR LAS GUIAS TAMBIEN
+
+"""
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+# Token del bot
+BOT_TOKEN = "tu_token_aqui"
+
+# Comando para enviar el chat ID
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await update.message.reply_text(f"Tu chat ID es: {chat_id}")
+
+def main():
+    # Crear aplicación
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    # Agregar manejador de comandos
+    application.add_handler(CommandHandler("start", start))
+
+    # Iniciar el bot
+    application.run_polling()  # Usa run_polling directamente
+
+if __name__ == "__main__":
+    main()
+
+"""
