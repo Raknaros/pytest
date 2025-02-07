addcfc9ebcbce7d95e7384912de4947a8ae1d9c3
Raknaros <giumarchand@gmail.com>
2025-02-07 11:52:59 -0500

    Pedidos y facturas rdy

--- OrdenarArchivos.py
+++ OrdenarArchivos.py
@@ -6,14 +6,14 @@ import paramiko
 
 """
 
-FICHA RUC = 'reporteec_ficharuc_{ruc}_{YYYYMMDDHHMMSS}'
-RESOLUCION DE INTENDENCIA (INGRESO COMO RECAUDACION) = 'ridetrac_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'
-RESOLUCION DE INTENDENCIA (LIBERACION DE FONDOS) = 'rilf_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'
-RESOLUCION DE MULTA = 'rmgen_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'
-CONSTANCIA DE NOTIFICACION = 'constancia_{YYYYMMDDHHMMSS}_%PORDEFINIR%_{numero_resolucion}_%PORDEFINIR%}'
-RESOLUCION COACTIVA = 'rvalores_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'
-COMUNICACION DE BAJA DE INSCRIPCION DE OFICIO DEFINITIVA (formulario 2660) = 'bod_%PORDEFINIR%_{ruc}_{numero_formulario}'
-FACTURA PDF = 'PDF-DOC-{numero_serie}-{numero_correlativo}{ruc}'
+FICHA RUC = 'reporteec_ficharuc_{ruc}_{YYYYMMDDHHMMSS}'9_8_11_14=42
+RESOLUCION DE INTENDENCIA (INGRESO COMO RECAUDACION) = 'ridetrac_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'8_11_13_14_9=59
+RESOLUCION DE INTENDENCIA (LIBERACION DE FONDOS) = 'rilf_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'4_11_13_14_9=55
+RESOLUCION DE MULTA = 'rmgen_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'5_11_3-3-7_14_9=58
+CONSTANCIA DE NOTIFICACION = 'constancia_{YYYYMMDDHHMMSS}_%PORDEFINIR%_{numero_resolucion}_%PORDEFINIR%}'10_14_20_13_9=70
+RESOLUCION COACTIVA = 'rvalores_{ruc}_{numero_resolucion}_{YYYYMMDDHHMMSS}_%PORDEFINIR%'8_11_X_14_9=46
+COMUNICACION DE BAJA DE INSCRIPCION DE OFICIO DEFINITIVA (formulario 2660) = 'bod_%PORDEFINIR%_{ruc}_{numero_formulario}'3_6_11_4=27
+FACTURA PDF = 'PDF-DOC-{numero_serie}-{numero_correlativo}{ruc}'=3-3-4-X-11=
 FACTURA XML = 'FACTURA{numero_serie}-{numero_correlativo}{ruc}'
 BOLETA DE VENTA PDF = 'PDF-BOLETA{numero_serie}-{numero_correlativo}{ruc}'
 NOTA DE CREDITO PDF = 'PDF-NOTA_CREDITO{numero_serio}{numero_correlativo}{ruc}'
@@ -21,7 +21,6 @@ RECIBO POR HONORARIOS PDF = 'RHE{ruc}{numero_serie}{numero_correlativo}'
 GUIA DE REMISION REMITENTE PDF = '{ruc}-{tipo_documento}-{numero_serie}-{numero_correlativo}'
 REPORTE PLANILLA T REGISTRO ZIP = '{ruc}_{codigo_reporte}_{DDMMYYYY}
 DETALLE DECLARACIONES Y PAGOS EXCEL = 'DetalleDeclaraciones_{ruc}_%PORDEFINIR%'
-
 
 """
 
@@ -31,18 +30,35 @@ DETALLE DECLARACIONES Y PAGOS EXCEL = 'DetalleDeclaraciones_{ruc}_%PORDEFINIR%'
 #SI ES RESOLUCION, ENVIAR A RESOLUCIONES
 #SI ES REPORTE, ENVIAR A REPORTES
 #MOVER TODOS DE UBICACION A LA UBICACION CONVENIENTE(CENTRAL-SERVER, EC2 O A ESTA LAPTOP
-
+inicios_sunat = ['reporteec_ficharuc_',
+                 'ridetrac_',
+                 'rilf_',
+                 'rmgen_',
+                 'constancia_',
+                 'rvalores_',
+                 'bod_',
+                 'PDF-DOC',
+                 'FACTURA',
+                 'PDF-BOLETA',
+                 'PDF-NOTA_CREDITO',
+                 'RHE',
+                 'DetalleDeclaraciones',
+                 '',
+                 '',
+                 ''
+                ]
 def analizar_archivos(directorio, extension):
     archivos_encontrados = []
     for root, dirs, files in os.walk(directorio):
         for file in files:
-            if file.endswith(extension):
-                archivos_encontrados.append(os.path.join(root, file))
-            if file.endswith('.zip'):
-                zip_file = zipfile.ZipFile(os.path.join(root, file))
-                for zip_info in zip_file.infolist():
-                    if zip_info.filename.endswith(extension):
-                        archivos_encontrados.append(os.path.join(root, file) + ':' + zip_info.filename)
+            for inicio in inicios_sunat:
+                if file.endswith(extension):
+                    archivos_encontrados.append(os.path.join(root, file))
+                if file.endswith('.zip'):
+                    zip_file = zipfile.ZipFile(os.path.join(root, file))
+                    for zip_info in zip_file.infolist():
+                        if zip_info.filename.endswith(extension):
+                            archivos_encontrados.append(os.path.join(root, file) + ':' + zip_info.filename)
     return archivos_encontrados
 
 
